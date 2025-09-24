2691188a6e7eceb96364ab7fac98b9c7c8076aea
Heavenfiend <EGS-76@yandex.ru>
2025-09-24 14:21:51 +0300

    удалена папка OOP2 из-за ненадобности

--- OOP2/README.md
+++
@@ -1,1 +0,0 @@
-<img width="1047" height="89" alt="image" src="https://github.com/user-attachments/assets/e6918cd0-0adb-456e-ac2b-eb3e57d08376" />
--- OOP2/main.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Егор on 9/18/2025.
-//
-#include <iostream>
-
-int main() {
-    std::cout << "Hello from CMake project!\n";
-    return 0;
-}
